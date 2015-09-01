50c37fe7eb4c7e5abf6f978607b6fb663b953c15
lhmagic <lhmagic@139.com>
2015-09-01 23:07:21 +0800

    Merge: generate readme file code.

--- Recorder.ino
+++ Recorder.ino
@@ -38,11 +38,7 @@ String str;
 
 void gen_readme(void) {
 String msg = "\
-<<<<<<< HEAD
 \r\n数据每秒记录一次(无效的数据用'#'表示)，包含的内容有：\r\n\
-=======
-\r\n数据每秒记录一次，包含的内容有：\r\n\
->>>>>>> 69ef0a3923f1d29198c169d8d921f0cb7e12238b
 \r\n当前时间 | 纬度(°) | 经度(°) | 海拔(m) | 速度(km/h) | 朝向(°) | 温度(℃) | 气压(Pa) | [x,y,z]加速度(m/s2) | [x,y,z]角速度(°/s) | 心率(times/min)";  
   if(!file_exist("README.TXT")) {
     Serial.println("Readme file does not exist, create it.");
